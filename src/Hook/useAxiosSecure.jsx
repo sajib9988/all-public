6eb05d9ee8b12c0625a0d22b203a1515aaf1f561
sajib9988 <sajib.biswas9988@gmail.com>
2024-09-03 21:37:31 +0600

    add logic

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -28,10 +28,10 @@ const useAxiosSecure = () => {
     }, async (error) => {
         const status = error?.response?.status;        // console.log('status error in the interceptor', status);
         // for 401 or 403 logout the user and move the user to the login
-        if (status === 401 || status === 403) {
-            await logOut();
-            navigate('/login');
-        }
+        // if (status === 401 || status === 403) {
+        //     await logOut();
+        //     navigate('/login');
+        // }
         return Promise.reject(error);
     })
 
